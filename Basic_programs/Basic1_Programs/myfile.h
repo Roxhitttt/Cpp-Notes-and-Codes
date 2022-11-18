7d91091fa576cbe71787ec457562f61447e6e020
Rohit Gosavi <rohit02032001@gmail.com>
2022-11-18 18:44:46 +0530

    cpp

---
+++ Basic_programs/Basic1_Programs/myfile.h
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+void add(int a, int b)
+{
+    int c=a+b;
+    cout<<"addition of"<<a<<" and "<<b<<" is "<<c;
+    // This file is .h which is stored in the library of C++ so that it can be used in the .cpp files.
+}
---
+++ Basic_programs/Basic1_Programs/pre_procsessor.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "myfile.h"     // This file is store in the library of C++ so that it can be used in this file
+#define PI 3.14;       // This defile header file is used to make the constant value throught the program.
+using namespace std;
+int main()
+{
+    add(5,6);
+    cout<<endl;
+    cout<<"Pi is "<<PI;
+    return 0;
+    
+}
